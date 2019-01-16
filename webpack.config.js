13714f7259d4f4abace377d8697ed1da97d0a70a
Ryan Blackman <rblackman24@gmail.com>
2019-01-16 10:42:37 -0600

    remove externals

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,6 @@ module.exports = {
 	entry: {
 		hello: path.join(__dirname, '/ts/hello.tsx'),
 	},
-	externals: {
-		'react': 'React',
-		'react-dom': 'ReactDOM'
-	},
 	output: {
 		path: path.resolve(__dirname, "js"),
 		filename: "[name].js",
